aa6db07a8085aa9be3897d7874725798db98bc65
sushil.gaikwad <sushil.gaikwad@gwle.onmicrosoft.com>
2021-08-11 16:55:35 +0100

    Added Component for Movies List & Service for data

--- MovieListFrontend/src/app/app.component.spec.ts
+++ MovieListFrontend/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MovieListFrontend'`, () => {
+  it(`should have as title 'My Movies'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('MovieListFrontend');
+    expect(app.title).toEqual('My Movies');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to MovieListFrontend!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to My Movies!');
   });
 });
--- MovieListFrontend/src/app/app.module.ts
+++ MovieListFrontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoviesListComponent } from './movies-list/movies-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesListComponent
   ],
   imports: [
     BrowserModule,
