ff220853e5656acdc88558ebf404b1939dcf7fcf
sushil.gaikwad <sushil.gaikwad@gwle.onmicrosoft.com>
2021-08-11 21:51:18 +0100

    Added 2nd call to BE to fetch other movie detail

--- MovieListFrontend/src/app/services/movies.service.ts
+++ MovieListFrontend/src/app/services/movies.service.ts
@@ -3,17 +3,27 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { MoviesResponse } from '../models/moviesList/Response';
+import { MovieDetailsResponse } from '../models/movieDetails/MovieDetailsResponse';
+
+const _url = "https://localhost:44364/api/MovieInformation";
+const _urlDetails = "https://localhost:44364/api/MovieInformation/GetMovieInformation";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
-  private _url = "https://localhost:44364/api/MovieInformation";
 
   constructor(private http: HttpClient) { }
 
   getMovies(): Observable<MoviesResponse>{
-    return this.http.get<MoviesResponse>(this._url).pipe(catchError((error: HttpErrorResponse) =>{
+    return this.http.get<MoviesResponse>(_url).pipe(catchError((error: HttpErrorResponse) =>{
+                                                    return throwError(error.message || 'server error');
+                                                  }));
+  }
+
+  getMovieDetails(title: string): Observable<MovieDetailsResponse>{
+    const _urlDetailsUpdated = `${_urlDetails}/${title}`;
+    return this.http.get<MovieDetailsResponse>(_urlDetailsUpdated).pipe(catchError((error: HttpErrorResponse) =>{
                                                     return throwError(error.message || 'server error');
                                                   }));
   }
