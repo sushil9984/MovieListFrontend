c73c668d69f748cfd9fdabf4aeef08238501852e
sushil.gaikwad <sushil.gaikwad@gwle.onmicrosoft.com>
2021-08-11 17:59:23 +0100

    Updated packages, routing, sample table

--- MovieListFrontend/src/app/app-routing.module.ts
+++ MovieListFrontend/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MoviesListComponent } from './movies-list/movies-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'movies', component:MoviesListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [MoviesListComponent]
